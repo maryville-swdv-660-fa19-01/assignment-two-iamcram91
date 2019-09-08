fd73eb3b782a6b22545bf97098ced5a51495ed45
Marc Kirksey <mkirksey1@live.maryville.edu>
2019-09-08 17:47:01 -0500

    bug-fix-t1
    Bug fix for clearing AssertionError for test_game_model.py
    
    Bug #1 fixed

--- spaceman/tests/test_game_model.py
+++ spaceman/tests/test_game_model.py
@@ -52,7 +52,7 @@ class GameModelTests( TestCase ):
         )
 
         game.handleGuess('X')
-        self.assertEquals( expectedGuessesTaken, game.guesses_taken )
+        self.assertEquals( expectedGuessesTaken + 1, game.guesses_taken )
     
 
     ### guessed_word_state field
